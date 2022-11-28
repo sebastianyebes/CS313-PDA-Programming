a5eb58ac19becf9803faf0f5bfc70726baef90ed
Carl Sebastian Yebes <carlsebastian.yebes@cit.edu>
2022-11-28 21:50:22 +0800

    Implement GetAlgorithm

--- CS313-PDA-Programming/CS313-PDA-Programming/Factories/PDAFactory.cs
+++ CS313-PDA-Programming/CS313-PDA-Programming/Factories/PDAFactory.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CS313_PDA_Programming.Algorithms;
+using CS313_PDA_Programming.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,17 @@ namespace CS313_PDA_Programming.Factories
 {
     public class PDAFactory
     {
+        public static IAlgorithm GetAlgorithm(int input)
+        {
+            switch (input)
+            {
+                case 1:
+                    return new PalindromeAlgo();
+                case 2:
+                    return new BalancingParenthesesAlgo();
+                default:
+                    throw new Exception("Invalid Input..");
+            }
+        }
     }
 }
