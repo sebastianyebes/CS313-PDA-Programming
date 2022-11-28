cd47dbcdb94f793077e0c0b4a9f5068310b3866f
Carl Sebastian Yebes <carlsebastian.yebes@cit.edu>
2022-11-29 01:27:27 +0800

    Implement method

--- CS313-PDA-Programming/CS313-PDA-Programming/Algorithms/BalancingParenthesesAlgo.cs
+++ CS313-PDA-Programming/CS313-PDA-Programming/Algorithms/BalancingParenthesesAlgo.cs
@@ -6,7 +6,34 @@ namespace CS313_PDA_Programming.Algorithms
     {
         public void implement(string input)
         {
-            throw new NotImplementedException();
+            Stack<char> stack = new Stack<char>();
+            int length = input.Length;
+
+            for(int i = 0; i < length; i++)
+            {
+                if(input[i] == '(' || input[i] == '{' || input[i] == '['){
+                    Console.WriteLine("Pushed: " + input[i]);
+                    stack.Push(input[i]);
+                    continue;
+                }
+
+                if(input[i] == ')' && stack.Peek() == '(' || 
+                   input[i] == ']' && stack.Peek() == '[' ||
+                   input[i] == '}' && stack.Peek() == '{')
+                {
+                    Console.WriteLine("Popped: " + input[i]);
+                    stack.Pop();
+                }
+            }
+
+            if (stack.Count == 0)
+            {
+                Console.WriteLine("\nInput is Balanced, therefore it is Accepted!");
+            }
+            else
+            {
+                Console.WriteLine("\nInput is Unbalanced, therefore it is not Accepted!");
+            }
         }
     }
 }
