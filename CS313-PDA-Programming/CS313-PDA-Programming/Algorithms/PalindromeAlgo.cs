eb64ef2022b308886bb0f4b892b7d88fc5f31f90
Carl Sebastian Yebes <carlsebastian.yebes@cit.edu>
2022-11-29 00:47:41 +0800

    Add Documentation

--- CS313-PDA-Programming/CS313-PDA-Programming/Algorithms/PalindromeAlgo.cs
+++ CS313-PDA-Programming/CS313-PDA-Programming/Algorithms/PalindromeAlgo.cs
@@ -1,12 +1,10 @@
 ﻿using CS313_PDA_Programming.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CS313_PDA_Programming.Algorithms
 {
+    /// <summary>
+    /// Solves the input wether if the input is accepted as a palindrome or not
+    /// </summary>
     internal class PalindromeAlgo : IAlgorithm
     {
         public void implement(string input)
@@ -20,7 +18,8 @@ namespace CS313_PDA_Programming.Algorithms
             {
                 throw new Exception("Input is null");
             }
-            else if(length % 2 == 0)
+            // if |input| is even, then push a & b, if |input| is already in the half part, then pop a on a & pop b on b
+            else if (length % 2 == 0) 
             {
                 for (int i = 0; i < length; i++)
                 {
@@ -38,7 +37,8 @@ namespace CS313_PDA_Programming.Algorithms
                     }
                 }
             }
-            else if(length % 2 != 0)
+            // if |input| is odd, then push a & b, if |input| is already in the middle, then skip wether its a or b, then after the middle, you pop a on a & pop b on b
+            else if (length % 2 != 0) 
             {
                 for (int i = 0; i < length; i++)
                 {
@@ -62,6 +62,7 @@ namespace CS313_PDA_Programming.Algorithms
                 }
             }
 
+            // if the stack is empty, then it means the input is a palindrome
             if(stack.Count == 0)
             {
                 Console.WriteLine("\nInput is Accepted!");
