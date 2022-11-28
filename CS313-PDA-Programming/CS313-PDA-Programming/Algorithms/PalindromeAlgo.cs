dba5db5fb8040d6e3e24ec7e6071a42d61eff6e1
Carl Sebastian Yebes <carlsebastian.yebes@cit.edu>
2022-11-28 22:58:38 +0800

    Implement method

--- CS313-PDA-Programming/CS313-PDA-Programming/Algorithms/PalindromeAlgo.cs
+++ CS313-PDA-Programming/CS313-PDA-Programming/Algorithms/PalindromeAlgo.cs
@@ -11,7 +11,65 @@ namespace CS313_PDA_Programming.Algorithms
     {
         public void implement(string input)
         {
-            throw new NotImplementedException();
+            Stack<char> stack = new Stack<char>();
+            int length = input.Length;
+
+            Console.WriteLine("\nProcess:");
+
+            if (length == 0)
+            {
+                throw new Exception("Input is null");
+            }
+            else if(length % 2 == 0)
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    if (i < length / 2)
+                    {
+                        Console.WriteLine("Pushed: " + input[i]);
+                        stack.Push(input[i]);
+                        continue;
+                    }
+                    
+                    if(stack.Peek() == input[i])
+                    {
+                        Console.WriteLine("Popped: " + input[i]);
+                        stack.Pop();
+                    }
+                }
+            }
+            else if(length % 2 != 0)
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    if (i < length / 2)
+                    {
+                        Console.WriteLine("Pushed: " + input[i]);
+                        stack.Push(input[i]);
+                        continue;
+                    }
+                    else if(i == length / 2)
+                    {
+                        Console.WriteLine("Read and Skipped: " + input[i]);
+                        continue;
+                    }
+
+                    if (stack.Peek() == input[i])
+                    {
+                        Console.WriteLine("Popped: " + input[i]);
+                        stack.Pop();
+                    }
+                }
+            }
+
+            if(stack.Count == 0)
+            {
+                Console.WriteLine("\nInput is Accepted!");
+            }
+            else
+            {
+                Console.WriteLine("\nInput is not Accepted!");
+            }
         }
     }
 }
