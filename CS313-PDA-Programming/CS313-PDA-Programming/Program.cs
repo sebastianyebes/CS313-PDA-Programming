69e44358cfe77c6c0d6cb1b4d8137d23983ede3d
Carl Sebastian Yebes <carlsebastian.yebes@cit.edu>
2022-11-28 23:33:24 +0800

    Implement Program.cs

--- CS313-PDA-Programming/CS313-PDA-Programming/Program.cs
+++ CS313-PDA-Programming/CS313-PDA-Programming/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CS313_PDA_Programming;
+using CS313_PDA_Programming.Factories;
+using CS313_PDA_Programming.Interfaces;
+
+class Program
+{
+    public static void Main()
+    {
+        Console.Write("Programs:\n1 - Palindrome\n2 - Balancing Parantheses\n\nAnswer: ");
+        var input = Console.ReadLine();
+        int program = Convert.ToInt32(input);
+
+        Console.Clear();
+        if (program == 1)
+        {
+            Console.WriteLine("PALINDROME\n");
+        }
+        else if (program == 2)
+        {
+            Console.WriteLine("BALANCING PARANTHESES\n");
+        }
+
+        Console.Write("Input: ");
+        input = Console.ReadLine();
+        IAlgorithm algorithm = PDAFactory.GetAlgorithm(program);
+        algorithm.implement(input);
+    }
+}
