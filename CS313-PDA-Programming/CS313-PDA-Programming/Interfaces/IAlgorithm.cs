7ea28f73c3770724de8951a41268c9a02d5d9e30
Carl Sebastian Yebes <carlsebastian.yebes@cit.edu>
2022-11-28 21:36:13 +0800

    Create interface and its methods

---
+++ CS313-PDA-Programming/CS313-PDA-Programming/Interfaces/IAlgorithm.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS313_PDA_Programming.Interfaces
+{
+    public interface IAlgorithm
+    {
+        public void implement();
+    }
+}
